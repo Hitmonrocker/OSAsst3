f2edeb05678a90970984734a6997d73c616cfac9
Steve F <sflynn218@yahoo.com>
2017-12-08 22:13:44 -0500

    added sfs_unlink()

--- sfs.c
+++ sfs.c
@@ -100,7 +100,7 @@ typedef struct _inode {
 static inode* root;
 
 void *sfs_init(struct fuse_conn_info *conn)
-{	
+{
 	struct sfs_state* state = SFS_DATA;
 
 	char* disk=state->diskfile;
@@ -166,7 +166,7 @@ void *sfs_init(struct fuse_conn_info *conn)
 
     fprintf(stderr, "in bb-init\n");
     log_msg("\nsfs_init()\n");
-    
+
     log_conn(conn);
     log_fuse_context(fuse_get_context());
 
@@ -197,7 +197,7 @@ int sfs_getattr(const char *path, struct stat *statbuf)
 {
     int retstat = 0;
     //char fpath[PATH_MAX];
-    
+
     log_msg("\nsfs_getattr(path=\"%s\", statbuf=0x%08x)\n",
 	  path, statbuf);
 
@@ -320,18 +320,53 @@ int sfs_create(const char *path, mode_t mode, struct fuse_file_info *fi)
     int retstat = 0;
     log_msg("\nsfs_create(path=\"%s\", mode=0%03o, fi=0x%08x)\n",
 	    path, mode, fi);
-    
-    
+
+
     return retstat;
 }
 
 /** Remove a file */
 int sfs_unlink(const char *path)
 {
+		inode* myRoot=NULL;
+		inode* cursor=NULL;
     int retstat = 0;
     log_msg("sfs_unlink(path=\"%s\")\n", path);
 
-    
+		//find root i-node
+		struct sfs_state* state = SFS_DATA;
+		char* file=state->diskfile;
+
+		disk_open(file);
+		block_read(0, myRoot);
+		//find the i-node associated with that filehandle
+		int i=1;
+		int j=0;
+		for(i=1; i < 1000; i++){
+			//get i-node from memory
+			block_read(i, cursor);
+			//if the paths match the i-node was found
+			//reset the i-node then write back to file
+			if(strcmp(cursor->path,path) == 0){
+				cursor->mode=2;
+				cursor->size=-1;
+				cursor->userId=getuid();
+				cursor->groupId=-getegid();
+				//cursor->path=NULL;
+				cursor->timeStamp=time(NULL);
+				cursor->permissions=-1;
+				//TODO:update free array-->single and double indirection
+				cursor->singleIndirectionPtrs[0]=-1;
+				cursor->doubleIndirectionPtrs[0]=-1;
+				for(j;j<100;j++) {
+					freeArray[cursor->directMappedPtrs[j]]=0;
+					cursor->directMappedPtrs[j]=-1;
+				}
+				//write the updated cursor back to disk
+				block_write(i, cursor);
+			}
+		}
+		disk_close();
     return retstat;
 }
 
@@ -351,7 +386,7 @@ int sfs_open(const char *path, struct fuse_file_info *fi)
     log_msg("\nsfs_open(path\"%s\", fi=0x%08x)\n",
 	    path, fi);
 
-    
+
     return retstat;
 }
 
@@ -374,7 +409,7 @@ int sfs_release(const char *path, struct fuse_file_info *fi)
     int retstat = 0;
     log_msg("\nsfs_release(path=\"%s\", fi=0x%08x)\n",
 	  path, fi);
-    
+
 
     return retstat;
 }
@@ -395,7 +430,7 @@ int sfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse
     int retstat = 0;
     log_msg("\nsfs_read(path=\"%s\", buf=0x%08x, size=%d, offset=%lld, fi=0x%08x)\n",
 	    path, buf, size, offset, fi);
-   
+
     return retstat;
 }
 
@@ -413,8 +448,8 @@ int sfs_write(const char *path, const char *buf, size_t size, off_t offset,
     int retstat = 0;
     log_msg("\nsfs_write(path=\"%s\", buf=0x%08x, size=%d, offset=%lld, fi=0x%08x)\n",
 	    path, buf, size, offset, fi);
-    
-    
+
+
     return retstat;
 }
 
@@ -425,8 +460,8 @@ int sfs_mkdir(const char *path, mode_t mode)
     int retstat = 0;
     log_msg("\nsfs_mkdir(path=\"%s\", mode=0%3o)\n",
 	    path, mode);
-   
-    
+
+
     return retstat;
 }
 
@@ -437,8 +472,8 @@ int sfs_rmdir(const char *path)
     int retstat = 0;
     log_msg("sfs_rmdir(path=\"%s\")\n",
 	    path);
-    
-    
+
+
     return retstat;
 }
 
@@ -455,8 +490,8 @@ int sfs_opendir(const char *path, struct fuse_file_info *fi)
     int retstat = 0;
     log_msg("\nsfs_opendir(path=\"%s\", fi=0x%08x)\n",
 	  path, fi);
-    
-    
+
+
     return retstat;
 }
 
@@ -483,11 +518,11 @@ int sfs_opendir(const char *path, struct fuse_file_info *fi)
  */
 int sfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset,
 	       struct fuse_file_info *fi)
-{	
+{
 	 log_msg("\nreaddir\n");
     int retstat = 0;
-    
-    
+
+
     return retstat;
 }
 
@@ -499,7 +534,7 @@ int sfs_releasedir(const char *path, struct fuse_file_info *fi)
 {
     int retstat = 0;
 
-    
+
     return retstat;
 }
 
@@ -535,7 +570,7 @@ int main(int argc, char *argv[])
 	printf("p node size : %d\n", sizeof(pnode) );
     int fuse_stat;
     struct sfs_state *sfs_data;
-    
+
     // sanity checking on the command line
     if ((argc < 3) || (argv[argc-2][0] == '-') || (argv[argc-1][0] == '-'))
 	sfs_usage();
@@ -551,13 +586,13 @@ int main(int argc, char *argv[])
     argv[argc-2] = argv[argc-1];
     argv[argc-1] = NULL;
     argc--;
-    
+
     sfs_data->logfile = log_open();
-    
+
     // turn over control to fuse
     fprintf(stderr, "about to call fuse_main, %s \n", sfs_data->diskfile);
     fuse_stat = fuse_main(argc, argv, &sfs_oper, sfs_data);
     fprintf(stderr, "fuse_main returned %d\n", fuse_stat);
-    
+
     return fuse_stat;
 }
